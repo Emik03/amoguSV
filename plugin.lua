5c551b277809cd7becced64dca9f9ad9b133ba76
Emik <emik.lakunishok@gmail.com>
2024-10-08 19:36:49 +0200

    Increase max SV points

--- plugin.lua
+++ plugin.lua
@@ -47,7 +47,7 @@ BEEG_BUTTON_SIZE = {255, 24}        -- beeg button
 MIN_RGB_CYCLE_TIME = 6              -- minimum seconds for one complete RGB color cycle
 MAX_RGB_CYCLE_TIME = 300            -- maximum seconds for one complete RGB color cycle
 MAX_CURSOR_TRAIL_POINTS = 100       -- maximum number of points for cursor trail effects
-MAX_SV_POINTS = 1000                -- maximum number of SV points allowed
+MAX_SV_POINTS = 10000               -- maximum number of SV points allowed
 MAX_ANIMATION_FRAMES = 999          -- maximum number of animation frames allowed
 MAX_IMPORT_CHARACTER_LIMIT = 999999 -- maximum number of characters allowed for import text
 
